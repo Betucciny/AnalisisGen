ec3ba2ee91666bde1f4313e2bdbd8e1f3536ed7e
Roberto Herrera <86085514+Betucciny@users.noreply.github.com>
2023-03-26 16:04:15 -0600

    pso

--- main.py
+++ main.py
@@ -3,18 +3,11 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    path = 'diffB/dif'
-    binarios, tablab = archivo2array(path)
-    mediab = np.mean(binarios, axis=0)
-    print(mediab)
-
-    path = 'diffE/difE'
-    reales, tablar = archivo2array(path)
-    mediar = np.mean(reales, axis=0)
-    print(mediar)
-    print(tablar)
-    for nl, linea in enumerate(tablar):
-        print(nl+1, end=' & ')
+    path = 'PSO/pso'
+    FO, tabla = archivo2array(path)
+    FO_mean = np.mean(FO, axis=0)
+    for nl, linea in enumerate(tabla):
+        print(nl + 1, end=' & ')
         for nc, num in enumerate(linea):
             if nc == len(linea) - 1:
                 print(num, end=' \\\\')
@@ -22,25 +15,27 @@ def main():
                 print(num, end=' & ')
         print()
 
-    x = np.arange(1, 201, 1)
-    plt.plot(x, mediab, label='Binomial')
-    plt.plot(x, mediar, label='Exponencial')
+
+    x = np.arange(1, 501)
+    plt.plot(x, FO_mean, label='Binomial')
     plt.show()
+    plt.clf()
+
+    plt.plot(x[:50], FO_mean[:50], label='Binomial')
+    plt.show()
+    plt.clf()
+
 
 
 def archivo2array(path):
-    datos = []
+    FO = []
     tabla = []
     for i in range(30):
-        archivo = open(path + '(' + str(i + 1) + ').txt', 'r')
-        corrida = archivo.readline().split()
-        var = archivo.readline().split()
-        fo = archivo.readline().split()
-        tabla.append(var+fo)
-        datos.append(corrida)
-        archivo.close()
-    datos = np.array(datos, dtype=float)
-    return datos, tabla
+        archivoFO = open(path + '(' + str(i + 1) + ').txt', 'r')
+        datos = archivoFO.readline().split()
+        FO.append(datos)
+        archivoFO.close()
+    return np.array(FO, dtype=float), tabla
 
 
 if __name__ == '__main__':
